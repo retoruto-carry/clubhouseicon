fdf103ea0f290cf4fee3bd4099ad4e8ebf7d8b52
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2021-02-11 05:28:59 +0900

    下地を作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,32 @@
 <template>
-  <div class="container">
+  <div class="container mx-auto py-8 px-8">
     <div>
-      <h1 class="text-4xl">👋 Clubhouse アイコンメーカー</h1>
-      <p class="">Coming soon...</p>
+      <header>
+        <h1 class="text-4xl text-center">👋 Clubhouse アイコンメーカー</h1>
+      </header>
+      <main class="mt-8">
+        <section>
+          <h2 class="heading">✏ テキスト</h2>
+          <input
+            class="w-full h-10 px-3 mb-2 text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline mt-2"
+            type="text"
+            placeholder="睡眠中"
+          />
+        </section>
+        <section class="mt-8">
+          <h2 class="heading">🎨 スタイル</h2>
+        </section>
+        <section class="mt-8">
+          <h2 class="heading">🖼 ボーダー</h2>
+        </section>
+        <section class="mt-12 text-center">
+          <button
+            class="rounded-full bg-brand-accent text-white py-4 px-12 text-xl"
+          >
+            <span class="mr-2">💾 </span>保存
+          </button>
+        </section>
+      </main>
     </div>
   </div>
 </template>
@@ -14,7 +38,7 @@ export default Vue.extend({})
 </script>
 
 <style scoped>
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+.heading {
+  @apply text-2xl;
 }
 </style>
