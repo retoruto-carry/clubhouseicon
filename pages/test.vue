d0e6be42b257583bfdf26d2fe63239f699c524bc
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2021-02-11 12:09:56 +0900

    refactor svg

--- pages/test.vue
+++ pages/test.vue
@@ -1,51 +1,78 @@
 <template>
   <div>
-    <svg id="svg" width="300" height="300" viewBox="0 0 300 300">
-      <!-- クリップパス用SVG -->
-      <svg width="0" height="0" style="position: absolute; top: 0; left: 0">
-        <clipPath id="clip01">
-          <path
-            d="M100 50C100 85.7361 85.7361 100 50 100C14.2639 100 0 85.7361 0 50C0 14.2639 14.2639 0 50 0C85.7361 0 100 14.2639 100 50Z"
-          />
-        </clipPath>
-      </svg>
-      <!-- 実際の表示用SVG -->
-      <svg id="svg2" width="300" height="300" viewBox="0 0 100 100">
-        <image
-          xlink:href="/images/icon.jpeg"
-          width="100%"
-          height="100%"
-          preserveAspectRatio="xMidYMid slice"
-          clip-path="url(#clip01)"
-        />
+    <svg id="svg" width="300" height="300" viewBox="0 0 100 100">
+      <defs>
         <path
-          fill="#000000"
-          fill-opacity="0.3"
+          id="path01"
           d="M100 50C100 85.7361 85.7361 100 50 100C14.2639 100 0 85.7361 0 50C0 14.2639 14.2639 0 50 0C85.7361 0 100 14.2639 100 50Z"
         />
-        <text
-          x="50"
-          y="50"
-          text-anchor="middle"
-          dominant-baseline="central"
-          style="font-size: 20px; fill: #fff"
-        >
-          聞き専
-        </text>
-      </svg>
+        <clipPath id="clip01">
+          <use xlink:href="#path01" />
+        </clipPath>
+      </defs>
+      <image
+        :href="url"
+        width="100%"
+        height="100%"
+        preserveAspectRatio="xMidYMid slice"
+        clip-path="url(#clip01)"
+      />
+      <use xlink:href="#path01" fill="#000000" fill-opacity="0.3" />
+      <text
+        x="50"
+        y="50"
+        text-anchor="middle"
+        dominant-baseline="central"
+        style="font-size: 20px; fill: #fff"
+      >
+        {{ text }}
+      </text>
     </svg>
-    <button @click="downloadImage">DL</button>
+    <input
+      type="file"
+      accept="image/jpg,image/png,image/gif"
+      @change="handleInputImage"
+    />
+    <input v-model="text" />
+    <button
+      class="rounded-full bg-brand-accent text-white py-2 px-8 text-md"
+      @click="downloadImage"
+    >
+      <span class="mr-2">💾 </span>保存
+    </button>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+
+type HTMLInputEvent = Event & {
+  target: HTMLInputElement & EventTarget
+}
+type LocalData = {
+  url: string
+  text: string
+}
+
 const ssap = require('save-svg-as-png')
 
 export default Vue.extend({
+  data(): LocalData {
+    return {
+      url: '',
+      text: '',
+    }
+  },
   methods: {
     downloadImage() {
       ssap.saveSvgAsPng(document.getElementById('svg'), 'download.png')
+    },
+    handleInputImage(e: HTMLInputEvent) {
+      if (!e.target.files?.length) {
+        return
+      }
+      const file = e.target.files[0]
+      this.url = window.URL.createObjectURL(file)
     },
   },
 })
