fec8844f7cba15ab8fa22b1e8cad9249cc75ff41
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2021-02-11 14:19:44 +0900

    トップページに実装

---
+++ components/Icon.vue
@@ -0,0 +1,86 @@
+<template>
+  <svg
+    id="svg"
+    ref="icon"
+    xmlns="http://www.w3.org/2000/svg"
+    :width="width"
+    :height="width"
+    viewBox="0 0 100 100"
+  >
+    <defs>
+      <path
+        id="path01"
+        d="M100 50C100 85.7361 85.7361 100 50 100C14.2639 100 0 85.7361 0 50C0 14.2639 14.2639 0 50 0C85.7361 0 100 14.2639 100 50Z"
+      />
+      <clipPath id="clip01">
+        <use xlink:href="#path01" />
+      </clipPath>
+    </defs>
+    <use xlink:href="#path01" :fill="borderColor" />
+    <image
+      :xlink:href="url"
+      width="100%"
+      height="100%"
+      preserveAspectRatio="xMidYMid slice"
+      clip-path="url(#clip01)"
+      transform="scale(0.9 0.9)"
+      transform-origin="center"
+    />
+    <use
+      xlink:href="#path01"
+      fill="#000000"
+      fill-opacity="0.25"
+      transform="scale(0.9 0.9)"
+      transform-origin="center"
+    />
+    <text
+      x="50"
+      y="50"
+      text-anchor="middle"
+      dominant-baseline="central"
+      style="font-size: 20px; fill: #fff"
+    >
+      {{ text }}
+    </text>
+  </svg>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+const ssap = require('save-svg-as-png')
+
+export default Vue.extend({
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+    borderColor: {
+      type: String,
+      default: '',
+    },
+    url: {
+      type: String,
+      default: '',
+    },
+    width: {
+      type: Number,
+      default: 100,
+    },
+  },
+  methods: {
+    downloadImage() {
+      ssap.saveSvgAsPng(this.$refs.icon, 'download.png', {
+        scale: 10,
+      })
+    },
+  },
+})
+</script>
+
+<style scoped>
+.preview {
+  width: 100px;
+  height: 100px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,25 +5,46 @@
         <h1 class="md:text-2xl text-xl">
           <span class="mr-2">👋 </span>Clubhouse アイコンメーカー
         </h1>
+        <p class="text-xm text-center">(現在開発中)</p>
       </header>
-      <main class="mt-8">
+      <main class="mt-4">
         <section>
+          <Icon
+            ref="icon"
+            :url="url"
+            :text="text"
+            :border-color="borderColor"
+            :width="150"
+            class="mx-auto"
+          />
+          <input
+            class="mt-4"
+            type="file"
+            accept="image/jpg,image/png,image/gif"
+            @change="handleInputImage"
+          />
+        </section>
+        <section class="mt-8">
           <h2 class="heading"><span class="mr-2">✏</span> テキスト</h2>
           <input
-            class="w-full h-10 px-3 mb-2 text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline mt-2"
+            v-model="text"
+            class="w-full h-10 px-3 text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline mt-2"
             type="text"
-            placeholder="睡眠中"
+            placeholder="聞き専"
           />
         </section>
         <section class="mt-8">
           <h2 class="heading"><span class="mr-2">🎨</span> スタイル</h2>
+          Coming soon...
         </section>
         <section class="mt-8">
           <h2 class="heading"><span class="mr-2">🖼</span> ボーダー</h2>
+          <input v-model="borderColor" type="color" />
         </section>
-        <section class="mt-12 text-center">
+        <section class="mt-8 text-center">
           <button
             class="rounded-full bg-brand-accent text-white py-2 px-8 text-md"
+            @click="handleSaveImage"
           >
             <span class="mr-2">💾 </span>保存
           </button>
@@ -36,7 +57,37 @@
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+type HTMLInputEvent = Event & {
+  target: HTMLInputElement & EventTarget
+}
+type LocalData = {
+  url: string
+  text: string
+  borderColor: string
+}
+
+export default Vue.extend({
+  data(): LocalData {
+    return {
+      url: '',
+      text: '',
+      borderColor: '#fafafa',
+    }
+  },
+  methods: {
+    handleInputImage(e: HTMLInputEvent) {
+      if (!e.target.files?.length) {
+        return
+      }
+      const file = e.target.files[0]
+      this.url = window.URL.createObjectURL(file)
+    },
+    handleSaveImage() {
+      const icon = this.$refs.icon as any
+      icon.downloadImage()
+    },
+  },
+})
 </script>
 
 <style scoped>
