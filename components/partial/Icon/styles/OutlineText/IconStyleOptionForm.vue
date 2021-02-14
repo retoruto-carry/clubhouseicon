e81be0bf495be94241464c72cd489bdcc118a8e5
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2021-02-14 15:18:39 +0900

    縁取り文字を追加

---
+++ components/partial/Icon/styles/OutlineText/IconStyleOptionForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h3 class="inline-block text-xs">文字の色</h3>
+    <input
+      class="align-bottom"
+      :value="value.fontColor"
+      type="color"
+      @input="handleUpdateFontColor"
+    />
+    <h3 class="inline-block text-xs">縁の色</h3>
+    <input
+      class="align-bottom"
+      :value="value.outlineColor"
+      type="color"
+      @input="handleUpdateOutlineColor"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+import { OutlineTextIconStyleOption } from '~/types/icon'
+
+type HTMLInputEvent = Event & {
+  target: HTMLInputElement & EventTarget
+}
+
+export default Vue.extend({
+  props: {
+    value: {
+      type: Object as PropType<OutlineTextIconStyleOption>,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      localValue: this.value,
+    }
+  },
+  methods: {
+    handleUpdateOutlineColor(event: HTMLInputEvent) {
+      this.localValue!.outlineColor = event.target.value as string
+      this.$emit('input', this.localValue)
+    },
+    handleUpdateFontColor(event: HTMLInputEvent) {
+      this.localValue!.fontColor = event.target.value as string
+      this.$emit('input', this.localValue)
+    },
+  },
+})
+</script>
---
+++ components/partial/Icon/styles/OutlineText/TopLayer.vue
@@ -0,0 +1,94 @@
+<template>
+  <svg>
+    <text
+      ref="measurementTextBox"
+      x="50"
+      y="50"
+      text-anchor="middle"
+      dominant-baseline="central"
+      :style="`font-size: 20px; fill: #000; font-weight: bold; opacity: 0`"
+      font-family="sans-serif"
+    >
+      {{ text }}
+    </text>
+    <defs>
+      <text
+        id="outText"
+        x="50"
+        y="50"
+        text-anchor="middle"
+        dominant-baseline="central"
+        font-family="sans-serif"
+        :style="baseStyle"
+      >
+        {{ text }}
+      </text>
+    </defs>
+    <use
+      ref="showTextBox"
+      xlink:href="#outText"
+      :style="outlineTextStyle"
+    ></use>
+    <use xlink:href="#outText" :style="innerTextStyle"></use>
+  </svg>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+import { OutlineTextIconStyleOption } from '~/types/icon'
+
+type LocalData = {
+  fontSize: number
+}
+
+export default Vue.extend({
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+    styleOption: {
+      type: Object as PropType<OutlineTextIconStyleOption>,
+      required: true,
+    },
+  },
+  data(): LocalData {
+    return {
+      fontSize: 20,
+    }
+  },
+  computed: {
+    baseStyle(): string {
+      return `font-weight: bold; font-size: ${this.fontSize}px; stroke: ${this.styleOption.outlineColor}; fill: ${this.styleOption.fontColor}; letter-spacing: 0.2rem;`
+    },
+    outlineTextStyle(): string {
+      return `stroke-width: ${
+        this.fontSize / 4
+      }px; paint-order: stroke; stroke-linejoin: round;`
+    },
+    innerTextStyle() {
+      return 'stroke-width: 0;'
+    },
+  },
+  mounted() {
+    this.adjustFontSize()
+  },
+  updated() {
+    this.adjustFontSize()
+  },
+  methods: {
+    adjustFontSize() {
+      if (this.text === '') return
+      const measurementTextBox = this.$refs.measurementTextBox as any
+      const showTextBox = this.$refs.showTextBox as any
+      const measurementTextBoxBBox = measurementTextBox.getBBox()
+      const showTextBoxBBox = showTextBox.getBBox()
+      if (measurementTextBoxBBox.width > 98) {
+        if (showTextBoxBBox.width > 98) this.fontSize = this.fontSize - 2
+      } else {
+        this.fontSize = 20
+      }
+    },
+  },
+})
+</script>
